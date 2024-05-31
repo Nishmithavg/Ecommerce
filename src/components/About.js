f0bcfaafc71b5a69139d0de36a8ea6c2a0d60e1f
Nishmithavg <nishmithavg@gmail.com>
2024-05-31 12:08:39 +0530

    first push

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h5>SHOPIFY STORY</h5>
+      <h2>The first Shopify store was our own</h2>
+      <h5>Over a decade ago, we started a store to sell snowboards online. None of the ecommerce solutions at the time gave us the control we needed to be successful—so we built our own. Today, businesses of all sizes use Shopify, whether they’re selling online, in retail stores, or on-the-go.</h5>
+      <center><img src=".\images.jpg"width="1200" height="600"></img></center>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1><center>Easily Build and Run Your Ecommerce Website</center></h1><br></br>
+      <br></br>
+     
+      <h3><center>Shopify is better than any other platform we've played with, and we've played with them all.”
+
+Jonathan Bayme, CEO of Theory11</center></h3>
+<br></br>
+<br></br>
+<center><img src=".\download.png"></img></center>
+
+    </div>
+  );
+}
+
+export default Home;
